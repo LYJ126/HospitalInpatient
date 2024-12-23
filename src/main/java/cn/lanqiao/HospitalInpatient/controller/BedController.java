bac09b8d38e44a0908b78131dc0986e7775239a5
LYJ126 <murongqingyu2726@gmail.com>
2024-12-23 12:16:47 +0800

    基础文件创建

--- src/main/java/cn/lanqiao/HospitalInpatient/controller/BedController.java
+++ src/main/java/cn/lanqiao/HospitalInpatient/controller/BedController.java
@@ -1,9 +1,15 @@
 package cn.lanqiao.HospitalInpatient.controller;
 
+import cn.lanqiao.HospitalInpatient.service.BedService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @Slf4j
 public class BedController {
+
+    @Autowired
+    private BedService bedService;
+
 }
