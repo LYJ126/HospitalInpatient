44c1c9831106ce36eab36508b7ea5a151c1b5000
LYJ126 <148759623+LYJ126@users.noreply.github.com>
2024-12-23 09:29:40 +0800

    Delete src/main/java/cn/lanqiao/HospitalInpatient/Test directory

--- src/main/java/cn/lanqiao/HospitalInpatient/Test/test4.java
+++
@@ -1,7 +0,0 @@
-package cn.lanqiao.HospitalInpatient.Test;
-
-public class test4 {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
--- src/main/java/cn/lanqiao/HospitalInpatient/Test/test5.java
+++
@@ -1,7 +0,0 @@
-package cn.lanqiao.HospitalInpatient.Test;
-
-public class test5 {
-    public static void main(String[] args) {
-        System.out.println(133);
-    }
-}
