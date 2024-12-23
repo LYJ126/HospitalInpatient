cf97bb8ef9ef6e19ddd2fb5357e15455423a9443
LYJ126 <murongqingyu2726@gmail.com>
2024-12-23 15:42:06 +0800

    完成了登录类的后端代码创建

---
+++ src/main/java/cn/lanqiao/HospitalInpatient/service/LoginuserService.java
@@ -0,0 +1,7 @@
+package cn.lanqiao.HospitalInpatient.service;
+
+import cn.lanqiao.HospitalInpatient.pojo.Loginuser;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface LoginuserService extends IService<Loginuser> {
+}
---
+++ src/main/java/cn/lanqiao/HospitalInpatient/service/impl/LoginuserServiceImpl.java
@@ -0,0 +1,9 @@
+package cn.lanqiao.HospitalInpatient.service.impl;
+
+import cn.lanqiao.HospitalInpatient.mapper.LoginuserMapper;
+import cn.lanqiao.HospitalInpatient.pojo.Loginuser;
+import cn.lanqiao.HospitalInpatient.service.LoginuserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+public class LoginuserServiceImpl extends ServiceImpl<LoginuserMapper, Loginuser> implements LoginuserService {
+}
