fdf6983859ec72139bb2d4d8474711210c610082
LYJ126 <murongqingyu2726@gmail.com>
2024-12-24 10:05:14 +0800

    修改了index网页的部分内容

--- src/main/java/cn/lanqiao/HospitalInpatient/controller/LoginuserController.java
+++ src/main/java/cn/lanqiao/HospitalInpatient/controller/LoginuserController.java
@@ -3,21 +3,22 @@ package cn.lanqiao.HospitalInpatient.controller;
 import cn.lanqiao.HospitalInpatient.service.LoginuserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
-@RestController
-@RequestMapping("/Login")
+@Controller
 public class LoginuserController {
 
     @Autowired
     private LoginuserService loginuserService;
 
-    @PostMapping("/index")
-    public String login() {
-        return "index.html";
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password) {
+        System.out.println(username);
+        System.out.println(password);
+        return "/index";
     }
 
 }
