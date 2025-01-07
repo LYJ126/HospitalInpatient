662d06ca623ac1c620e5f6ff6b7307c0e120a692
z <3489935607@qq.com>
2025-01-07 09:30:32 +0800

    config解决cors问题,cost新增指定查询

--- src/main/java/cn/lanqiao/HospitalInpatient/config/WebConfig.java
+++ src/main/java/cn/lanqiao/HospitalInpatient/config/WebConfig.java
@@ -3,6 +3,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//  配置跨域
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
