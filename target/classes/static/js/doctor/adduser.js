4f214847a775c959e24ee4123c362332b9af42cc
LYJ126 <148759623+LYJ126@users.noreply.github.com>
2025-01-07 11:48:52 +0800

    Delete target directory

--- target/classes/static/js/doctor/adduser.js
+++
@@ -1,9 +0,0 @@
-function checkform() {
-    if (document.addForm.name.value === "" || document.addForm.number.value === "") {
-        alert("请输入完整信息！");
-        return false;
-    } else {
-        alert("添加成功！");
-        return true;
-    }
-}
