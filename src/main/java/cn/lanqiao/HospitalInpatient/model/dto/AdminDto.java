73bd4c204b75f23e8ef998316f576bd35a69af2c
LYJ126 <murongqingyu2726@gmail.com>
2025-01-09 12:33:08 +0800

    救回来了

--- src/main/java/cn/lanqiao/HospitalInpatient/model/dto/AdminDto.java
+++ src/main/java/cn/lanqiao/HospitalInpatient/model/dto/AdminDto.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+/**
+ * @author: LYJ
+ * @date: 2022/11/26 16:51
+ * @description: 修改密码
+ */
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class AdminDto {
     private String username;
-    private String password;
+    private String originalPassword;
+    private String newPassword;
 }
